305c29f53b8959b174e97d258a864e7b348e9780
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-01-04 17:04:58 +0700

    dto

--- cqrs-pattern/src/main/java/id/ten/example/cqrspattern/dto/OrderCommandDto.java
+++ cqrs-pattern/src/main/java/id/ten/example/cqrspattern/dto/OrderCommandDto.java
@@ -1,3 +1,23 @@
 package id.ten.example.cqrspattern.dto;
+
 public class OrderCommandDto {
+
+    private int userIndex;
+    private int productIndex;
+
+    public int getUserIndex() {
+        return userIndex;
+    }
+
+    public void setUserIndex(int userIndex) {
+        this.userIndex = userIndex;
+    }
+
+    public int getProductIndex() {
+        return productIndex;
+    }
+
+    public void setProductIndex(int productIndex) {
+        this.productIndex = productIndex;
+    }
 }
